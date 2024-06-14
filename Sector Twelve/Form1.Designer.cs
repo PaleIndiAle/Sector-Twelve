9f2abbbe3d6faf21c033fce81447638bf409c362
Kevnel <kevichen510@gmail.com>
2024-06-14 13:57:40 -0700

    Add project files.

---
+++ Sector Twelve/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Sector_Twelve
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.subtitleLabel = new System.Windows.Forms.Label();
+            this.descLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(12, 165);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(375, 40);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "label1";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // subtitleLabel
+            // 
+            this.subtitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.subtitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtitleLabel.ForeColor = System.Drawing.Color.White;
+            this.subtitleLabel.Location = new System.Drawing.Point(12, 205);
+            this.subtitleLabel.Name = "subtitleLabel";
+            this.subtitleLabel.Size = new System.Drawing.Size(375, 45);
+            this.subtitleLabel.TabIndex = 1;
+            this.subtitleLabel.Text = "label1";
+            this.subtitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // descLabel
+            // 
+            this.descLabel.BackColor = System.Drawing.Color.Transparent;
+            this.descLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.descLabel.ForeColor = System.Drawing.Color.Black;
+            this.descLabel.Location = new System.Drawing.Point(12, 42);
+            this.descLabel.Name = "descLabel";
+            this.descLabel.Size = new System.Drawing.Size(375, 236);
+            this.descLabel.TabIndex = 2;
+            this.descLabel.Text = "label1";
+            this.descLabel.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(15, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 30);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 30);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 401);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.descLabel);
+            this.Controls.Add(this.subtitleLabel);
+            this.Controls.Add(this.titleLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label subtitleLabel;
+        private System.Windows.Forms.Label descLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Sector Twelve/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+using System.Media;
+using System.Threading;
+
+namespace Sector_Twelve
+{
+    public partial class Form1 : Form
+    {
+        int c = 0;
+        int m;
+        int x, y;
+
+        Rectangle background = new Rectangle(0, 0, 800, 800);
+
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush blackBrush = new SolidBrush(Color.Black);
+        Pen blackPen = new Pen(Color.Black);
+        SolidBrush blueBrush = new SolidBrush(Color.Blue);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+
+        new List<Rectangle> ballList = new List<Rectangle>();
+        new List<string> ballColour = new List<string>();
+
+        Rectangle Ralph = new Rectangle(160, 360, 40, 40);
+
+        Rectangle ZombA = new Rectangle(120, 280, 40, 40);
+        Rectangle ZombB = new Rectangle(240, 360, 40, 40);
+        Rectangle ZombC = new Rectangle(240, 200, 40, 40);
+        Rectangle ZombD = new Rectangle(240, 120, 40, 40);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void InitializeGame()
+        {
+            gameTimer.Enabled = true;
+            titleLabel.Text = "";
+            subtitleLabel.Text = "";
+            //time = 500;
+            //score = 0;
+            ballColour.Clear();
+
+            int y = 0;
+
+            for (int row = 0; row < 10; row++)
+            {
+                for (int col = 0; col <4; col++)
+                {
+                    int x = 120 + col * 40;
+
+                    ballList.Add(new Rectangle(x, y, 40, 40));
+                    ballColour.Add("white");
+                }
+
+                y += 40;
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Rectangle yellOne = new Rectangle(Ralph.X + 40, Ralph.Y, 40, 40);
+            Rectangle yellTwo = new Rectangle(Ralph.X - 40, Ralph.Y, 40, 40);
+            Rectangle yellThree = new Rectangle(Ralph.X, Ralph.Y + 40, 40, 40);
+
+            label1.Text = $"{x}, {y}";
+
+            int yOne = Ralph.Y + 40;
+            int yTwo = Ralph.Y - 40;
+
+            if (gameTimer.Enabled == false)
+            {
+                e.Graphics.FillRectangle(blackBrush, background);
+                titleLabel.Text = "Sector Twelve";
+                subtitleLabel.Text = "Press Space to Begin";
+            }
+            else if (c == 1)
+            {
+                descLabel.Visible = true;
+                descLabel.Text = "In an alternate 1981, HIV has mutated into a deadly virus which takes over their victim's mind before turning them into mindless husks of their former selves that attack all and spread the infection. You play as Ralph who is making his way to a military checkpoint to reach a safe zone established by the government. Don't get caught and make through the streets without being touched by these filths.";
+            }
+            else if (gameTimer.Enabled == true && c == 2)
+            {
+                descLabel.Text = "";
+                e.Graphics.FillRectangle(blackBrush, 0, 0, 120, this.Height);
+                e.Graphics.FillRectangle(whiteBrush, 120, 0, 160, this.Height);
+                e.Graphics.FillRectangle(blackBrush, 280, 0, 120, this.Height);
+
+                for (int i = 0; i < ballList.Count; i++)
+                {
+                    if (ballColour[i] == "white")
+                    {
+                        e.Graphics.FillRectangle(whiteBrush, ballList[i]);
+                    }
+                }
+
+                for (int i = 0; i < 10; i++)
+                {
+                    int x = i * 40;
+                    e.Graphics.DrawLine(blackPen, x, 0, x, this.Height);
+                }
+                for (int i = 0; i < 10; i++)
+                {
+                    int y = i * 40;
+                    e.Graphics.DrawLine(blackPen, 0, y, this.Width, y);
+                }
+
+                e.Graphics.FillRectangle(blueBrush, Ralph);
+                e.Graphics.FillRectangle(redBrush, ZombA);
+                e.Graphics.FillRectangle(redBrush, ZombB);
+                e.Graphics.FillRectangle(redBrush, ZombC);
+
+                if (Ralph.Y == 160)
+                {
+                    e.Graphics.FillRectangle(redBrush, ZombD);
+                }
+
+                if (m == 1)
+                {
+                    e.Graphics.FillRectangle(yellowBrush, yellOne);
+                    e.Graphics.FillRectangle(yellowBrush, yellTwo);
+                    e.Graphics.FillRectangle(yellowBrush, yellThree);
+                }
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            x = MousePosition.X - this.Location.X;
+            y = MousePosition.Y - this.Location.Y;
+
+            if (c == 3)
+            {
+                label2.Text = "gfaiugfajbf";
+            }
+
+            Refresh();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int x = 9;
+            switch(e.Button)
+            {
+                 
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Return:
+                    if (c == 1)
+                    {
+                        c = 2;
+                    }
+                    break;
+                case Keys.Space:
+                    if (c == 0)
+                    {
+                        c = 1;
+                        InitializeGame();
+                    }
+                    break;
+                case Keys.LButton:
+                    c = 3;
+                    break;
+            }
+        }
+    }
+}
